13a6c02b7d06ea6169b75a8e57fb786ee6d9080e
laibinglin <1154246882@qq.com>
2026-09-15 17:29:57 +0800

    获取数据

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
