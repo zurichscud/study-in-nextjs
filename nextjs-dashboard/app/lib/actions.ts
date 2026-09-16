eb813fb88e4e2ded9689d2ef6867b3e8c36b2795
laibinglin <1154246882@qq.com>
2026-09-16 17:12:57 +0800

    修正抛出错误

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -30,9 +30,9 @@ export async function createInvoice(formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
-    return {
-      message: "Database Error: Failed to Create Invoice.",
-    };
+    throw new Error("Database Error: Failed to Create Invoice.", {
+      cause: error,
+    });
   }
 
   revalidatePath("/dashboard/invoices");
@@ -58,7 +58,9 @@ export async function updateInvoice(id: string, formData: FormData) {
         WHERE id = ${id}
       `;
   } catch (error) {
-    return { message: "Database Error: Failed to Update Invoice." };
+    throw new Error("Database Error: Failed to Update Invoice.", {
+      cause: error,
+    });
   }
 
   revalidatePath("/dashboard/invoices");
@@ -70,9 +72,10 @@ export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
-    return { message: "Deleted Invoice." };
   } catch (error) {
-    return { message: "Database Error: Failed to Delete Invoice." };
+    throw new Error("Database Error: Failed to Delete Invoice.", {
+      cause: error,
+    });
   }
 }
 
